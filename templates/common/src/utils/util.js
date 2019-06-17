136c236b6ede72b9e4d2140de496133f9ac891a1
春田石雨 <flowke@163.com>
2019-06-17 19:25:21 +0800

    util

--- templates/common/src/utils/util.js
+++ templates/common/src/utils/util.js
@@ -93,6 +93,38 @@ export function toArr(obj){
   return val;
 }
 
+export function ordered(arr, op = {}) {
+  let { paraller, beforeEach, afterEach, waitingEach } = op;
+
+  let p = Promise.resolve(true);
+
+  if (paraller) {
+    arr = arr.map(fn => {
+
+      let out = fn()
+      beforeEach && beforeEach(out.info)
+      return () => out
+    })
+  }
+
+  return arr.reduce((acc, fn) => {
+
+    return acc.then(() => {
+      let out = fn();
+      if (!paraller) {
+        beforeEach && beforeEach(out.info)
+      }
+      waitingEach && waitingEach(out.info)
+      return out
+    })
+      .then(res => {
+        afterEach && afterEach(res)
+        return res
+      })
+  }, p)
+
+}
+
 export {
   getType
 }
