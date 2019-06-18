39ff0342a2893e125221a79a36ba84ba6cd1ef2e
春田石雨 <flowke@163.com>
2019-06-18 11:52:24 +0800

    util

--- templates/common/src/utils/util.js
+++ templates/common/src/utils/util.js
@@ -93,6 +93,11 @@ export function toArr(obj){
   return val;
 }
 
+// resolve promise by ordered
+// array<fn(return promise)>
+// op
+//  paraller: is run promise paraller
+//  
 export function ordered(arr, op = {}) {
   let { paraller, beforeEach, afterEach, waitingEach } = op;
 
@@ -123,6 +128,50 @@ export function ordered(arr, op = {}) {
       })
   }, p)
 
+}
+
+// 
+export function pieces(arr, options, callback){
+  let opType = getType(options);
+  let cbType = getType(callback);
+  let cb = null;
+
+  if (opType === 'function'){
+    cb = options;
+  } else if (cbType === 'function'){
+    cb = callback;
+  }else{
+    return;
+  }
+
+  let interval = 60,
+    piece = 10;
+
+
+  if (opType==='object'){
+    if (options.interval) interval = options.interval
+    if (options.piece) piece = options.piece
+  }
+
+
+  render(0, piece)
+
+  function render(begin, end){
+    if (end > arr.length) {
+      end = arr.length
+    }
+
+    cb(arr.slice(0, end), arr.slice(begin, end), arr);
+
+    if (end < arr.length) {
+      setTimeout(()=>{
+        render(end, end + piece)
+      }, interval)
+    }
+  }
+
+  
+  
 }
 
 export {
