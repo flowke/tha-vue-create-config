b050535dbb62c042a924d42d6c750b4b3bb8e09d
春田石雨 <flowke@163.com>
2019-08-07 12:15:24 +0800

    fit init

--- templates/router-vuex/src/init.js
+++
@@ -1,6 +0,0 @@
-
-
-export default function(Vue, router){
-  // write some code to init
-  // you can also return Promise , and vm will mount after promise resolve
-}
--- templates/simple/src/init.js
+++
@@ -1,6 +0,0 @@
-
-
-export default function(Vue){
-  // write some code to init
-  // you can also return Promise , and vm will mount after promise resolve
-}
